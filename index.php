56bf96daa6d48230f8f31bb51c7d85ce974f6d86
James Bennett <J.bennett@gmx.com>
2015-08-10 18:48:23 -0700

    final commit, adds full functionality and unique styling

--- index.php
+++ index.php
@@ -1,35 +1,47 @@
 <?php include 'database.php'; ?>
-<!DOCTYPE html>
-<html>
- <head>
-  <meta charset="UTF-8">
-  <title>JS Shoutbox</title>
-  <link rel="stylesheet" href="css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-  <script src="js/script.js"></script>
- </head>
+  <?php
+  // Create Select query
+  $query = "SELECT * FROM shouts ORDER BY id DESC";
+  $shouts = mysqli_query($con, $query);
+ ?>
+    <!DOCTYPE html>
+    <html>
 
- <body>
-   <div id="container">
-     <header>
-       <h1>JS Shoutbox</h1>
-     </header>
-     <div id="shouts">
-       <ul>
-         <li></li>
-       </ul>
+    <head>
+      <meta charset="UTF-8">
+      <title>JS Shoutbox</title>
+      <link rel="stylesheet" href="css/style.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+      <script src="js/script.js"></script>
+    </head>
 
-     </div>
-     <footer>
-       <form>
-         <label>Name: </label>
-         <input type="text" id="name">
-         <label>Shout Text</label>
-         <input type="text" id="shout">
-         <input type="submit" id="submit" value="SHOUT">
-       </form>
-     </footer>
-   </div>
+    <body>
+      <div id="container">
+        <header>
+          <h1>JS Shoutbox</h1>
+        </header>
+        <div id="shouts">
+          <ul>
+            <?php while($row = mysqli_fetch_assoc($shouts)) : ?>
+              <li>
+                <?php echo $row['name']; ?>:
+                  <?php echo $row['shout']; ?> [
+                    <?php echo $row['date'];?>]</li>
+              <?php endwhile; ?>
+          </ul>
 
- </body>
-</html>
+        </div>
+        <footer>
+          <form>
+            <label>Name: </label>
+            <input type="text" id="name">
+            <label>Shout Text</label>
+            <input type="text" id="shout">
+            <input type="submit" id="submit" value="SHOUT">
+          </form>
+        </footer>
+      </div>
+
+    </body>
+
+    </html>
